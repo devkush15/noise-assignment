ce2a7de88d3eda497ba8931024386fd5ac5c1449
devkush15 <135588806+devkush15@users.noreply.github.com>
2026-03-06 22:54:29 +0530

    Update test_scraper.py

--- test_scraper.py
+++ test_scraper.py
@@ -3,7 +3,7 @@ from ingestion.scraper import scrape_flipkart_reviews
 # Product 1 - Noise Master Buds Max
 url1 = "https://www.flipkart.com/noise-master-buds-max-sound-bose-segment-leading-anc-dynamic-eq-60-hr-playtime-bluetooth/product-reviews/itm583241394db27?pid=ACCHGZH5WCCGB3Y4&lid=LSTACCHGZH5WCCGB3Y4LI6ZZJ&marketplace=FLIPKART"
 
-# Product 2 - Noise Buds VS102 (or whatever Master Buds 1 URL you have)
+# Product 2 - Noise Master Buds 1 
 url2 = "https://www.flipkart.com/noise-master-buds-sound-bose-49db-anc-6-mic-enc-44-hr-battery-spatial-audio-bluetooth/product-reviews/itm965e6651f8980?pid=ACCHE4GYFJ8J2ADE&lid=LSTACCHE4GYFJ8J2ADEQWIFCY&marketplace=FLIPKART"
 
 print("Scraping Product 1...")
